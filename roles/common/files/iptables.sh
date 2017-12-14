1d7e989a79200cb4274b8ca6027d083d7162e0ec
Administrator <Administrator@P-V-4>
2017-12-14 12:53:27 +0800

    update

--- roles/common/files/iptables.sh
+++ roles/common/files/iptables.sh
@@ -3,11 +3,11 @@
 /sbin/iptables -A INPUT -i lo -j ACCEPT
 /sbin/iptables -A INPUT -p icmp -j ACCEPT
 /sbin/iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -A INPUT -i eno16777728 -p tcp --dport 22 -j ACCEPT
-/sbin/iptables -A INPUT -i eno16777728 -p tcp --dport 1080 -j ACCEPT
-/sbin/iptables -A INPUT -i eno16777728 -p tcp --dport 6080 -j ACCEPT
-/sbin/iptables -A INPUT -i eno16777728 -p tcp --dport 10050 -j ACCEPT
-/sbin/iptables -A INPUT -i eno16777728 -p tcp --dport 65530 -j ACCEPT
-/sbin/iptables -A INPUT -i eno33554952 -p tcp -s 172.16.0.0/24 -j ACCEPT
+/sbin/iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT
+/sbin/iptables -A INPUT -i eth0 -p tcp --dport 1080 -j ACCEPT
+/sbin/iptables -A INPUT -i eth0 -p tcp --dport 6080 -j ACCEPT
+/sbin/iptables -A INPUT -i eth0 -p tcp --dport 10050 -j ACCEPT
+/sbin/iptables -A INPUT -i eth0 -p tcp --dport 65530 -j ACCEPT
+/sbin/iptables -A INPUT -i eth3 -p tcp -s 172.16.0.0/24 -j ACCEPT
 /sbin/iptables -P INPUT DROP
 /sbin/iptables -P FORWARD DROP
