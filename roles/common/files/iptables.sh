8e0b651586d8f63ac0fa8e764f7b3635c27f3544
Administrator <Administrator@P-V-4>
2017-12-14 18:33:17 +0800

    update hosts

--- roles/common/files/iptables.sh
+++ roles/common/files/iptables.sh
@@ -3,17 +3,12 @@
 /sbin/iptables -A INPUT -i lo -j ACCEPT
 /sbin/iptables -A INPUT -p icmp -j ACCEPT
 /sbin/iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 22 -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 1080 -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 6080 -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 10050 -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 65530 -j ACCEPT
-/sbin/iptables -A INPUT -i inter_br0 -p tcp -s 192.168.142.0/24 -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 22 -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 1080 -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 6080 -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 10050 -j ACCEPT
-/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 65530 -j ACCEPT
-/sbin/iptables -A INPUT -i inter_br0 -p tcp -s 192.168.142.0/24 -j ACCEPT
+/sbin/iptables -A INPUT -i ens33 -p tcp --dport 22 -j ACCEPT
+/sbin/iptables -A INPUT -i ens33 -p tcp --dport 1080 -j ACCEPT
+/sbin/iptables -A INPUT -i ens33 -p tcp --dport 6080 -j ACCEPT
+/sbin/iptables -A INPUT -i ens33 -p tcp --dport 10050 -j ACCEPT
+/sbin/iptables -A INPUT -i ens33 -p tcp --dport 65530 -j ACCEPT
+/sbin/iptables -A INPUT -i ens35 -p tcp -s 192.168.142.0/24 -j ACCEPT
+
 /sbin/iptables -P INPUT DROP
 /sbin/iptables -P FORWARD DROP
