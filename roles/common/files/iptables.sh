ef85f3a6d53fa3803518b0420fab57f078cae6bd
Administrator <Administrator@P-V-4>
2017-12-14 13:40:34 +0800

    up

--- roles/common/files/iptables.sh
+++ roles/common/files/iptables.sh
@@ -9,5 +9,11 @@
 /sbin/iptables -A INPUT -i eth0 -p tcp --dport 10050 -j ACCEPT
 /sbin/iptables -A INPUT -i eth0 -p tcp --dport 65530 -j ACCEPT
 /sbin/iptables -A INPUT -i eth3 -p tcp -s 192.168.142.0/24 -j ACCEPT
+/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 22 -j ACCEPT
+/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 1080 -j ACCEPT
+/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 6080 -j ACCEPT
+/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 10050 -j ACCEPT
+/sbin/iptables -A INPUT -i ext_br0 -p tcp --dport 65530 -j ACCEPT
+/sbin/iptables -A INPUT -i inter_br0 -p tcp -s 192.168.142.0/24 -j ACCEPT
 /sbin/iptables -P INPUT DROP
 /sbin/iptables -P FORWARD DROP
